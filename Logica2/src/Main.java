0045b4d02e95e7dcb1314f66585a1a8cf5533a09
Nelson Costa <nelsonscosta@yahoo.com.br>
2026-02-17 10:43:25 -0300

    revisão com operador ternário

---
+++ Logica2/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Operador ternarios
+
+        int idade = 18;
+        double salario = 1300;
+
+        String mensagem = (idade > 17) ?"ele é maior de idade": "ele é de menor de idade";
+
+        System.out.println(mensagem);
+
+
+    }
+}
