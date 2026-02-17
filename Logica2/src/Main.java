d71d490765fe29e39a6d852b8fc53b9b211516b9
Nelson Costa <nelsonscosta@yahoo.com.br>
2026-02-17 10:56:50 -0300

    adicionado operado ternario

--- Logica2/src/Main.java
+++ Logica2/src/Main.java
@@ -2,14 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Operador ternarios
+        // Operador  ternarios
 
         int idade = 18;
-        double salario = 1300;
+        double salario = 1500;
 
         String mensagem = (idade > 17) ?"ele é maior de idade": "ele é de menor de idade";
 
         System.out.println(mensagem);
+
+        Double novoSalario = (salario <= 1300) ? (1300 * 0.20) +1300  : 1300 + 100;
+
+        System.out.println("Novo salário: "+novoSalario);
 
 
     }
