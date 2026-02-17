40c3f5d670337b943aca9c4e4d99283dbd9da087
Nelson Costa <nelsonscosta@yahoo.com.br>
2026-02-17 11:11:21 -0300

    adicionado mensagem completa

--- Logica2/src/Main.java
+++ Logica2/src/Main.java
@@ -4,16 +4,23 @@ public class Main {
     public static void main(String[] args) {
         // Operador  ternarios
 
-        int idade = 18;
+        String nome = "João";
+        int idade = 16;
         double salario = 1500;
 
         String mensagem = (idade > 17) ?"ele é maior de idade": "ele é de menor de idade";
 
-        System.out.println(mensagem);
+       // System.out.println(mensagem);
 
         Double novoSalario = (salario <= 1300) ? (1300 * 0.20) +1300  : 1300 + 100;
 
-        System.out.println("Novo salário: "+novoSalario);
+        // System.out.println("Novo salário: "+novoSalario);
+
+        String receberSalario = (idade >= 18) ? "vai receber o novo Salario": "não vai receber novo salario";
+
+       // System.out.println(receberSalario);
+
+        System.out.println(nome+" tem "+idade+" anos de idade, é "+mensagem+" e "+receberSalario+" no valor de "+"R$ "+novoSalario);
 
 
     }
